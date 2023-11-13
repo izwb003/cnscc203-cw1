dc38f2c25c400369766ba31c464d91683215862f
IZWB-003 <29999036+izwb003@users.noreply.github.com>
2023-11-14 03:55:44 +0800

    Change basic command line process by using argparse.
    Signed-off-by: IZWB-003 <29999036+izwb003@users.noreply.github.com>

--- ICMPPing.py
+++ ICMPPing.py
@@ -20,62 +20,15 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import sys
+import argparse
 
 version = '0.1.0'
 
 
-class CommandOptions:
-    """Get command line options and make proper settings."""
-    target_name = ""    # The target host name.
-    nonstop_flag = False    # If true, ping the specified host until stopped.
-
-    def __init__(self, argv):
-        if len(argv) == 1:
-            usage()
-        for args in argv:
-            if args is argv[0]:
-                continue
-            elif args == ("--help" or "-h"):
-                usage()
-                sys.exit(0)
-            elif args == ("--version" or "-V"):
-                show_version()
-                sys.exit(0)
-            elif args == "-t":
-                self.nonstop_flag = True
-            elif args is argv[len(argv) - 1]:
-                if args.startswith('-') or args.startswith('--'):
-                    print(f"Invalid argument {args}. Type \"--help\" or \"-h\" for help.")
-                    sys.exit(1)
-                self.target_name = args
-            else:
-                print(f"Invalid argument {args}. Type \"--help\" or \"-h\" for help.")
-                sys.exit(1)
-
-
-def usage():
-    """Show help information."""
-    global version
-    print(f"""ICMPPing.py {version} Copyright (c) 2023 Steven Song.
-Coursework for CNSCC203 Computer Networks 2023.
-
-USAGE:
-    ICMPPing [-h|--help] [-V|--version]
-    ICMPPing [-t][-l size]
-
-Options:
-    -h | --help           - Show help.
-    -V | --version        - Show version information.
-    -t                    - Ping the specified host until stopped.""")
-
-
-def show_version():
-    """Show version information."""
-    global version
-    print(f"""ICMPPing.py {version} Copyright (c) 2023 Steven Song.
-Coursework for CNSCC203 Computer Networks 2023.\n""")
-
-
 if __name__ == '__main__':
-    commandOptions = CommandOptions(sys.argv)
+    # Build command line process
+    commandParser = argparse.ArgumentParser(description=f"ICMPPing.py {version} Copyright (c) 2023 Steven Song. Coursework for CNSCC203 Computer Networks 2023.")
+    commandParser.add_argument('-t', action='store_true', default=False, help="ping the target host until stopped manually(Ctrl+C).")
+    commandParser.add_argument('target_host', type=str, help="target host to ping.")
+    commandOptions = commandParser.parse_args()
+    print(commandOptions)
