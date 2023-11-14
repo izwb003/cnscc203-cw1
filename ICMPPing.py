7131edea85ef0585393a713d67e0fa9708bfdd78
IZWB-003 <29999036+izwb003@users.noreply.github.com>
2023-11-14 18:12:29 +0800

    Build basic main program structure.
    Signed-off-by: IZWB-003 <29999036+izwb003@users.noreply.github.com>

--- ICMPPing.py
+++ ICMPPing.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
 """
 Copyright (c) 2023 Steven Song
 
@@ -21,14 +24,35 @@ SOFTWARE.
 """
 
 import argparse
+import socket
 
 version = '0.1.0'
+
+# class ICMPSocket:
+
+# def receiveOnePing(icmpSocket, destinationAddress, ID, timeout):
+
+# def sendOnePing(icmpSocket, destinationAddress, ID):
+
+# def doOnePing(destinationAddress, timeout):
+
+# def ping(host, timeout):
 
 
 if __name__ == '__main__':
     # Build command line process
     commandParser = argparse.ArgumentParser(description=f"ICMPPing.py {version} Copyright (c) 2023 Steven Song. Coursework for CNSCC203 Computer Networks 2023.")
     commandParser.add_argument('-t', action='store_true', default=False, help="ping the target host until stopped manually(Ctrl+C).")
-    commandParser.add_argument('target_host', type=str, help="target host to ping.")
+    commandParser.add_argument('-c', metavar='count', default=4, type=int, help="stop after <count> times.")
+    commandParser.add_argument('target_host', type=str, help="target host to ping. (DNS name or IP address)")
     commandOptions = commandParser.parse_args()
-    print(commandOptions)
+
+    # Do ping
+    if commandOptions.t:
+        while True:
+            print("TODO: set ping command here")
+            # ping(commandOptions.target_host)
+    else:
+        for i in range(0, commandOptions.c):
+            print("TODO: set ping command here")
+            # ping(commandOptions.target_host)
