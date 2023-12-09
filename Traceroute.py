1734d48703d998110136e19bee0f296feb0e8b92
IZWB-003 <29999036+izwb003@users.noreply.github.com>
2023-12-10 03:54:16 +0800

    Mark of a traceroute solution that does not work.
    Signed-off-by: IZWB-003 <29999036+izwb003@users.noreply.github.com>

--- Traceroute.py
+++ Traceroute.py
@@ -25,8 +25,72 @@ SOFTWARE.
 
 import argparse
 import socket
+import struct
+import time
+from ICMPChecksum import calculateChecksum
 
 version = '0.1.0'
+
+
+class TraceRoute4:
+    """
+    Define all the essential content for making a complete IPv4 Traceroute.
+    """
+    destinationAddress = ''
+    timeout = 3
+    icmpSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
+    icmpID = 1
+    icmpPacket = None
+
+    def __init__(self, targetHost, timeout):
+        self.destinationAddress = targetHost
+        self.timeout = timeout
+        self.icmpPacket = self.createICMPPacket()
+
+    def sendICMPRequest(self, ttl):
+        # Set TTL to IP header
+        self.icmpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+
+        # Send ICMP packet
+        # ICMP does not care about port, so 0 is OK.
+        self.icmpSocket.sendto(self.icmpPacket, (self.destinationAddress, 0))
+
+    def receiveICMPReply(self):
+        # Set ICMP timeout
+        self.icmpSocket.settimeout(self.timeout)
+
+        try:
+            # Try to receive ICMP reply
+            data, address = self.icmpSocket.recvfrom(1024)  # 1024 as the buffer size.
+            # Unpack the reply data
+            # Byte 20 to 27 is where the ICMP header locates in a packet.
+            icmp_header = struct.unpack("BBHHH", data[20:28])
+            # address[0] is the sender's IP address, icmp_header[0] is the message type.
+            return address[0], icmp_header[0]
+        except socket.timeout:
+            # If timeout return None
+            return None, None
+
+    def createICMPPacket(self):
+        """
+            Create ICMP Header as a "struct", fill with payload data.
+            Format:B as unsigned byte (8bit) for icmp_echo_request and icmp_echo_code;
+                   H as unsigned short int (16bit) for icmp_checksum, icmp_id and icmp_sequence;
+            [B:icmp_echo_request|B:icmp_echo_code|H:icmp_checksum|H:icmp_id|H:icmp_sequence][payload]
+            :return: The created ICMP packet.
+        """
+        icmp_echo_request = 8
+        icmp_echo_code = 0
+        icmp_checksum = 0
+        icmp_id = self.icmpID
+        icmp_sequence = 1
+
+        icmp_header = struct.pack("BBHHH", icmp_echo_request, icmp_echo_code, icmp_checksum, icmp_id, icmp_sequence)
+        icmp_checksum = calculateChecksum(icmp_header)
+        # Add 4 bytes to fit the size (64 bytes) of a ICMP packet. Works as payload.
+        icmp_packet = struct.pack("BBHHH", icmp_echo_request, icmp_echo_code, icmp_checksum, icmp_id, icmp_sequence) + b'\x00\x00\x00\x00'
+
+        return icmp_packet
 
 
 def traceroute(host, max_hops, timeout):
@@ -38,9 +102,35 @@ def traceroute(host, max_hops, timeout):
     :return:
     """
     destinationAddress = socket.gethostbyname(host)
+
     print(f"Tracking through up to {max_hops} hops")
     print(f"Routing to {host} [{destinationAddress}]:")
-    print("\n\n")
+
+    for ttl in range(1, max_hops + 1):
+        traceRoute = TraceRoute4(destinationAddress, timeout, ttl)
+        # Send ICMP request
+        traceRoute.sendICMPRequest()
+
+        # Record start time
+        startTime = time.time()
+
+        # Receive ICMP reply
+        address, icmpType = traceRoute.receiveICMPReply()
+
+        # Record end time and calculate time
+        endTime = time.time()
+        elapsedTime = int((endTime - startTime) * 1000)
+
+        # Close the socket
+        traceRoute.icmpSocket.close()
+        del traceRoute
+
+        if address:
+            print(f"{ttl}\t{address}\t{elapsedTime}ms")
+            if icmpType == 0:   # ICMP Type 0 means the message reached the target host, so break.
+                break
+        else:
+            print(f"{ttl}\t*\t{elapsedTime}ms")
 
 
 if __name__ == '__main__':
@@ -50,11 +140,11 @@ if __name__ == '__main__':
                     f"2023.")
     commandParser.add_argument('-j', metavar='maximum_hops', default=30, type=int,
                                help="The maximum number of hops for the search target.")
-    commandParser.add_argument('-w', metavar='timeout', default=3000, type=int,
+    commandParser.add_argument('-w', metavar='timeout', default=3, type=int,
                                help="Time out waiting for each reply (in milliseconds).")
-    commandParser.add_argument('target_host', type=str, help="target host to traceroute. (DNS name or IP address)")
+    commandParser.add_argument('target_host', type=str,
+                               help="target host to traceroute. (DNS name or IP address)")
     commandOptions = commandParser.parse_args()
 
     # Do traceroute
     traceroute(commandOptions.target_host, commandOptions.j, commandOptions.w)
-
